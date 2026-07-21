e0a2ef5d3778d781756493d442e1ded0c10cb0ac
eyob <eyob.tadesse@astu.edu.et>
2026-07-21 20:45:33 +0300

    refactor: remove unused repository annotation and update application configuration

--- user/src/main/java/com/service/user/repository/UserRepository.java
+++ user/src/main/java/com/service/user/repository/UserRepository.java
@@ -3,8 +3,6 @@ package com.service.user.repository;
 // import com.app.ecommerce.model.User;
 import com.service.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-// @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
